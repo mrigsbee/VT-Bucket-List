c897128ddc2eb1f0ff34a2866c9129925fdf00b2
Megan Rigsbee <mrigsbee@vt.edu>
2016-09-25 14:57:09 -0400

    Added toolbar and cleaned up formatting of ListView

--- app/src/main/java/mrigsbee/vtbucketlist/MainActivity.java
+++ app/src/main/java/mrigsbee/vtbucketlist/MainActivity.java
@@ -2,6 +2,8 @@ package mrigsbee.vtbucketlist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.EditText;
@@ -11,18 +13,28 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class MainActivity extends Activity{
+public class MainActivity extends AppCompatActivity {
 
     ListView list;
     ArrayList<String> items;
     Adapter adapter;
 
     ArrayList<Boolean> checkboxes;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null) {
+            setSupportActionBar(toolbar);
+            getSupportActionBar().setTitle(R.string.app_name);
+//            getSupportActionBar().setHomeButtonEnabled(true);
+//            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
 
         //set up lists
         items = new ArrayList<>();
